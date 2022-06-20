88fecf0134d40ee8c6dfd6ccbbf3c00727d2c8eb
John Wang <john-w@berkeley.edu>
2022-06-20 12:23:51 -0700

    first commit

---
+++ RandomPhoto/RandomPhoto/ViewController.swift
@@ -0,0 +1,19 @@
+ //
+//  ViewController.swift
+//  RandomPhoto
+//
+//  Created by John Wang on 6/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
