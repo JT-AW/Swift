65f487fd00287716af85f9550850269b78ad406c
John Wang <john-w@berkeley.edu>
2022-06-20 14:43:38 -0700

    Swift UI Second started

---
+++ Second/Second/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Second
+//
+//  Created by John Wang on 6/20/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        Image(/*@START_MENU_TOKEN@*/"Image Name"/*@END_MENU_TOKEN@*/)
+
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Second/Second/SecondApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecondApp.swift
+//  Second
+//
+//  Created by John Wang on 6/20/22.
+//
+
+import SwiftUI
+
+@main
+struct SecondApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
